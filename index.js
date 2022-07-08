af9fdd5414db4bde41a3988df6c96d817ef84554
Florent <florent.mathieu@gemini.com>
2022-07-08 14:51:09 -0400

    feat: add more goodies

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ export const chunk = (arr, chunkSize = 1, cache = []) => {
   return cache;
 };
 
-export const difference = (arr1, arr2) => arr1.filter((x) => arr2.includes(x));
+export const difference = (arr1, arr2) => arr1.filter((x) => !arr2.includes(x));
+
+const dropRight = (arr, n = 1) => arr.slice(0, -n || arr.length);
+
+const findLastIndex = (arr, func) => {
+  const reverseIdx = [...arr].reverse().findIndex(func);
+  return reverseIdx === -1 ? reverseIdx : arr.length - (reverseIdx + 1);
+};
