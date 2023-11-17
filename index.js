c3f9739e82d3bd58ec7301e26169549ed57b9e10
Florent <ze-flo@users.noreply.github.com>
2023-11-16 23:16:23 -1000

    fix(findLastIndex): add missing export

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ export const difference = (arr1, arr2) => arr1.filter((x) => !arr2.includes(x));
 
 export const dropRight = (arr, n = 1) => arr.slice(0, -n || arr.length);
 
-const findLastIndex = (arr, func) => {
+export const findLastIndex = (arr, func) => {
   const reverseIdx = [...arr].reverse().findIndex(func);
   return reverseIdx === -1 ? reverseIdx : arr.length - (reverseIdx - 1);
 };
