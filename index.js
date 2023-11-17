315686e70dea12628e67e047a1b3118460403c23
Florent <ze-flo@users.noreply.github.com>
2023-11-16 22:53:04 -1000

    fix(dropRight): add back missing export

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ export const chunk = (arr, chunkSize = 1, cache = []) => {
 
 export const difference = (arr1, arr2) => arr1.filter((x) => !arr2.includes(x));
 
-const dropRight = (arr, n = 1) => arr.slice(0, -n || arr.length);
+export const dropRight = (arr, n = 1) => arr.slice(0, -n || arr.length);
 
 const findLastIndex = (arr, func) => {
   const reverseIdx = [...arr].reverse().findIndex(func);
